b7f4256a5926f47dbc922dded63ef8ec6dd40e5d
Koplak9woi <168303349+Koplak9woi@users.noreply.github.com>
2024-08-13 13:51:49 +0700

    Create rfmok.sh

---
+++ rfmok.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+# Task 1: Recency calculation
+bq query --use_legacy_sql=false \
+"SELECT
+  user_id AS customer_id,
+  DATE_DIFF(CURRENT_TIMESTAMP(), MAX(created_at), DAY) AS recency
+FROM \`thelook_ecommerce.orders\`
+GROUP BY
+  user_id
+ORDER BY recency DESC
+LIMIT 10;" &
+echo "Task 1 started."
+
+# Task 2: Frequency calculation
+bq query --use_legacy_sql=false \
+"SELECT
+  user_id AS customer_id,
+  COUNT(order_id) as frequency
+FROM \`thelook_ecommerce.orders\`
+WHERE created_at >= '2022-01-01' AND created_at < '2023-01-01'
+GROUP BY customer_id
+ORDER BY frequency DESC
+LIMIT 10;" &
+echo "Task 2 started."
+
+# Task 3: Monetary calculation
+bq query --use_legacy_sql=false \
+"SELECT
+  o.user_id AS customer_id,
+  SUM(oi.sale_price) as monetary
+FROM \`thelook_ecommerce.orders\` o
+INNER JOIN \`thelook_ecommerce.order_items\` oi
+ON o.order_id = oi.order_id
+WHERE o.created_at >= '2022-01-01' AND o.created_at < '2023-01-01'
+GROUP BY customer_id
+LIMIT 10;" &
+echo "Task 3 started."
+
+# Task 4: RFM calculation
+bq query --use_legacy_sql=false \
+"WITH
+rfm_calc AS (
+SELECT
+  o.user_id AS customer_id,
+  DATE_DIFF(CURRENT_TIMESTAMP(), MAX(o.created_at), DAY) AS recency,
+  COUNT(o.order_id) AS frequency,
+  ROUND(SUM(oi.sale_price)) AS monetary
+FROM
+  \`thelook_ecommerce.orders\` o
+INNER JOIN
+  \`thelook_ecommerce.order_items\` oi
+ON
+  o.order_id = oi.order_id
+GROUP BY
+  customer_id
+)
+SELECT *
+FROM
+  rfm_calc;" &
+echo "Task 4 started."
+
+# Task 5: RFM segmentation
+bq query --use_legacy_sql=false \
+"WITH
+rfm_calc AS (
+SELECT
+  o.user_id AS customer_id,
+  DATE_DIFF(CURRENT_TIMESTAMP(), MAX(o.created_at), DAY) AS recency,
+  COUNT(o.order_id) AS frequency,
+  ROUND(SUM(oi.sale_price)) AS monetary
+FROM
+  \`thelook_ecommerce.orders\` o
+INNER JOIN
+  \`thelook_ecommerce.order_items\` oi
+ON
+  o.order_id = oi.order_id
+GROUP BY
+  customer_id
+),
+rfm_quant AS (
+SELECT
+  customer_id,
+  NTILE(4) OVER (ORDER BY recency) AS recency_quantile,
+  NTILE(4) OVER (ORDER BY frequency) AS frequency_quantile,
+  NTILE(4) OVER (ORDER BY monetary) AS monetary_quantile
+FROM
+  rfm_calc
+)
+SELECT
+  customer_id,
+  recency_quantile,
+  frequency_quantile,
+  monetary_quantile,
+  CASE
+    WHEN monetary_quantile >= 3 AND frequency_quantile >= 3 THEN 'High Value Customer'
+    WHEN frequency_quantile >= 3 THEN 'Loyal Customer'
+    WHEN recency_quantile <= 1 THEN 'At Risk Customer'
+    WHEN recency_quantile >= 3 THEN 'Persuadable Customer'
+  END AS customer_segment
+FROM
+  rfm_quant;" &
+echo "Task 5 started."
+
+# Wait for all background tasks to complete
+wait
+echo "All tasks completed."
